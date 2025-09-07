9edc627ba3efa25798685fe3ba80143791df898b
Jean Siabato <jeanikolas.js@gmail.com>
2025-09-07 22:10:33 +0200

    Construidos los botones para el histograma

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Sprint_7_df_automoviles
 Portafolio de Análisis de Datos de automóviles 
+
+Se realizo un note book con algunas anotaciones para recordar y guiar la app
+
+Habia conflicto con la instalacion de nbformat pero fue solucionado al instalar jupyter en la terminal
+
+En este momento ya estan construidos los botones del histograma
+Hace falta un encabezado con texto y Al menos un gráfico de dispersión, junto con Al menos un botón o una casilla de verificación.
+Las librerias que la aplicacion necesita estan en el documento de texto reuqeriments.txt
--- app_s7.py
+++ app_s7.py
@@ -6,4 +6,23 @@ import plotly.express as px
 #cargar datos / Load data
 df_car = pd.read_csv('vehicles_us.csv') 
 
-df_car = df_car.dropna(subset=['price', 'odometer']) # eliminar filas con valores nulos en las columnas 'price' y 'odometer'
+df_car = df_car.dropna(subset=['price', 'odometer']) # Si no hay valores en 'price' y 'odometer' no nos sirven para el analisis
+
+hist_button = st.button('Construir histograma') # crear un botón
+     
+if hist_button: # clic en el botón
+         # escribir un mensaje
+     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+     fig = px.histogram(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+     st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    
