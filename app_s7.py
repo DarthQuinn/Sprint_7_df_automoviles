9b8d774c6e5ea1c2bb8863e531026d866a7e25cd
Jean Siabato <jeanikolas.js@gmail.com>
2025-09-08 15:33:00 +0200

    Cambios en como se ve la aplicacion

--- app_s7.py
+++ app_s7.py
@@ -3,12 +3,33 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #1e3c72, #2a5298);
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("Bienvenido a mi app con fondo personalizado")
+st.write("¡El fondo ahora tiene un degradado azul oscuro!")
+
+
+
+st.header('demostracion de los conociemientos acerca de uso de desarollo de sofware para realizar paginas web')
+st.write('Esta paguna web solo muestra graficas sobre el conjunto de datos de automoviles')
+
 #"""Agregar un titulo y un encabezado"""
 
 st.header('Automobile Data Analysis') 
-st.write('Aqui debe verse una explicacion pequeña del tema')
+st.write('Los datos de los vehiculos aqui mostrados son obtenidos de la pagina web https://www.kaggle.com/datasets/austinreese/craigslist-carstrucks-data?resource=download')
 st.header('Segundo encabezado sobre los coches') 
-st.subheader('Subencabezado sobre los coches')
+st.subheader('Se muestran gracias combinando columnas del dataset, no hay una analisis riguroso al cual se quiera llegar o demostrar aqui')
 
 
 #cargar datos / Load data
@@ -17,10 +38,10 @@ df_car = pd.read_csv('vehicles_us.csv')
 
 df_car = df_car.dropna(subset=['price', 'odometer']) # Si no hay valores en 'price' y 'odometer' no nos sirven para el analisis
 
-"""
-Boton para hacer un histograma
-"""
 
+#Boton para hacer un histograma
+
+st.write('Construir un histograma para la columna odómetro:')
 hist_button = st.button('Construir histograma') # crear un botón
      
 if hist_button: # clic en el botón
@@ -33,16 +54,39 @@ if hist_button: # clic en el botón
          # mostrar un gráfico Plotly interactivo
      st.plotly_chart(fig, use_container_width=True)
 
-"""
-Crear una casilla de verificación
-"""
+#Boton para una segunda grafica de barras:
+st.write('Construir un gráfico de barras para la columna color:')
+bar_button = st.button('Construir gráfico de barras')
 
-build_histogram = st.checkbox('Construir el histograma')
+df_colorcar = df_car.groupby('paint_color').size()
 
-if build_histogram: # si la casilla de verificación está seleccionada
+if bar_button:
+     color_fig = px.bar(df_colorcar,
+                    x=df_colorcar.index,
+                    y=df_colorcar.values, 
+                    labels={'x': 'Color', 'y': 'Count'}, 
+                    title='Count of Cars by Color')
+     color_fig.update_traces(marker_color="#471fb4") #color morado
+     color_fig.show()
+
+#Crear una casilla de verificación
+
+build_scatter = st.checkbox('Construir el histograma')
+
+if build_scatter: # si la casilla de verificación está seleccionada
      st.write('Construir un histograma para la columna odómetro:')
 
-     fig_1 = px.histogram(df_car, x="odometer") # crear un histograma
-     st.plotly_chart(fig_1, use_container_width=True)
      fig_scatter = px.scatter(df_car, x="odometer", y="price") # crear un gráfico de dispersión
      fig_scatter.show() 
+
+
+color_scatter = st.checkbox('Construir gráfico de dispersión con el precio y los dias listados')
+
+if color_scatter: # si la casilla de verificación está seleccionada
+     days_scatter = px.scatter(df_car,
+                        x="days_listed",
+                        y="price",
+                        labels={'days_listed': 'Days Listed', 'price': 'Price'},
+                        title='Price vs Days Listed') 
+     days_scatter.update_traces(marker_color="#ee1d1d") 
+     days_scatter.show() 
